368335087db9878981d43c286a6083b297808437
Ellyll <cawsllyffant-git@yahoo.com>
2016-09-25 16:02:26 +0100

    Add jQuery

--- js/cellular.js
+++ js/cellular.js
@@ -63,53 +63,56 @@
         activeCells.forEach(cell => context.fillRect(cell.x, cell.y, cellWidth, cellHeight));
     }
 
+    function main() {
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
 
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
+        const numberOfColumns = 100; // 11
+        const cellSize = Math.floor(window.innerWidth / numberOfColumns);
+        const numberOfRows = Math.floor(window.innerHeight / cellSize);  //100; // 6
+        canvas.width = cellSize * numberOfColumns;
+        canvas.height = cellSize * numberOfRows;
 
-    const numberOfColumns = 100; // 11
-    const cellSize = Math.floor(window.innerWidth / numberOfColumns);
-    const numberOfRows = Math.floor(window.innerHeight / cellSize);  //100; // 6
-    canvas.width = cellSize * numberOfColumns;
-    canvas.height = cellSize * numberOfRows;
-
-    const rows = [];
-    for (let r = 0; r < numberOfRows; r++) {
-        let row;
-        if (r === 0) {
-            row = generateRow(numberOfColumns); //[0,0,0,0,0,1,0,0,0,0,0]
-        } else {
-            row = advanceRow(rows[r - 1]);
+        const rows = [];
+        for (let r = 0; r < numberOfRows; r++) {
+            let row;
+            if (r === 0) {
+                row = generateRow(numberOfColumns); //[0,0,0,0,0,1,0,0,0,0,0]
+            } else {
+                row = advanceRow(rows[r - 1]);
+            }
+            rows.push(row);
         }
-        rows.push(row);
+
+        let timeStep = 100; // speed = 1 cell / second
+        let lastTime = null;
+
+        let counter = 0;
+        let mainLoop = (currentTime, deltaTime) => {
+            counter++;
+            if (lastTime === null) {
+                rows.push(advanceRow(rows[rows.length-1]));
+                lastTime = currentTime;
+            }
+            let timeDiff = deltaTime + (currentTime - lastTime);
+            while (timeDiff >= timeStep) {
+                timeDiff -= timeStep;
+                rows.shift();
+                rows.push(advanceRow(rows[rows.length-1]));
+            }
+            let yOffSet = Math.floor(timeDiff*(cellSize/timeStep));
+            draw(context, rows, cellSize, cellSize, yOffSet);
+            lastTime = currentTime;
+            if (counter < 1000) {
+                window.requestAnimationFrame( (t) => mainLoop(t, timeDiff));
+            } else {
+                console.log('finished');
+            }
+        };
+        mainLoop(null, 0);
     }
 
-    let timeStep = 100; // speed = 1 cell / second
-    let lastTime = null;
-
-    console.log('timeStep', timeStep);
-
-    let counter = 0;
-    let mainLoop = (currentTime, deltaTime) => {
-        counter++;
-        if (lastTime === null) {
-            rows.push(advanceRow(rows[rows.length-1]));
-            lastTime = currentTime;
-        }
-        let timeDiff = deltaTime + (currentTime - lastTime);
-        while (timeDiff >= timeStep) {
-            timeDiff -= timeStep;
-            rows.shift();
-            rows.push(advanceRow(rows[rows.length-1]));
-        }
-        let yOffSet = Math.floor(timeDiff*(cellSize/timeStep));
-        draw(context, rows, cellSize, cellSize, yOffSet);
-        lastTime = currentTime;
-        if (counter < 1000) {
-            window.requestAnimationFrame( (t) => mainLoop(t, timeDiff));
-        } else {
-            console.log('finished');
-        }
-    };
-    mainLoop(null, 0);
+    $(document).ready(function() {
+        main();
+    });
 })();
