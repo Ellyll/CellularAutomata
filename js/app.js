9a78522d86bec60fc83284a5d17e3f5dde48935a
Ellyll <cawsllyffant-git@yahoo.com>
2016-10-01 17:32:02 +0100

    Split into app.js and lib/cellular.js

---
+++ js/app.js
@@ -0,0 +1,87 @@
+
+function main() {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    const numberOfColumns = 100; // 11
+    const cellSize = Math.floor(window.innerWidth / numberOfColumns);
+    const numberOfRows = Math.floor(window.innerHeight / cellSize);  //100; // 6
+    canvas.width = cellSize * numberOfColumns;
+    canvas.height = cellSize * numberOfRows;
+
+    const params = new URLSearchParams(location.search.slice(1));
+    const qsInitialValue = params.get('initialValue');
+    const initialRow = cellular.getRowFromQueryStringOrDefault(qsInitialValue, numberOfColumns, (n) => cellular.generateRow(n));
+    const hex = cellular.convertRowToHex(initialRow);
+    const $initialValue = $('#initialValue');
+    $initialValue.val(hex);
+    $initialValue.change(function() {
+        const value = $(this).val();
+        const isEnabled = cellular.isValidInitialValue(numberOfColumns, value);
+        //console.log(new Date(), `goButton enabled=${isEnabled} value=${value}`);
+        $('#goButton').prop('disabled', !isEnabled);
+    });
+
+    const rows = [];
+    for (let r = 0; r < numberOfRows; r++) {
+        let row;
+        if (r === 0) {
+            row = initialRow;
+        } else {
+            row = cellular.advanceRow(rows[r - 1]);
+        }
+        rows.push(row);
+    }
+
+    let isMenuActive = false;
+    let lastMoveTime = performance.now();
+
+    let timeStep = 100; // speed = 1 cell / second
+    let lastTime = null;
+
+    let counter = 0;
+    let mainLoop = (currentTime, deltaTime) => {
+        counter++;
+        if (isMenuActive && (currentTime - lastMoveTime)>2000) {
+            console.log('deactivating menu');
+            $('.menu').fadeOut();
+            isMenuActive = false;
+        }
+        if (lastTime === null) {
+            rows.push(cellular.advanceRow(rows[rows.length-1]));
+            lastTime = currentTime;
+        }
+        let timeDiff = deltaTime + (currentTime - lastTime);
+        while (timeDiff >= timeStep) {
+            timeDiff -= timeStep;
+            rows.shift();
+            rows.push(cellular.advanceRow(rows[rows.length-1]));
+        }
+        let yOffSet = Math.floor(timeDiff*(cellSize/timeStep));
+        cellular.draw(context, rows, cellSize, cellSize, yOffSet);
+
+        lastTime = currentTime;
+        if (counter < 1000) {
+            window.requestAnimationFrame( (t) => mainLoop(t, timeDiff));
+        } else {
+            console.log('finished');
+        }
+    };
+    mainLoop(null, 0);
+
+    $(document).mousemove(function() {
+        //console.log('moved', isMenuActive);
+        const currentTime = performance.now();
+        const elapsedTime = currentTime - lastMoveTime;
+        if (!isMenuActive) {
+            //console.log('activating menu');
+            $('.menu').fadeIn();
+            isMenuActive = true;
+        }
+        lastMoveTime = currentTime;
+    });
+}
+
+$(document).ready(function() {
+    main();
+});
--- js/lib/cellular.js
+++ js/lib/cellular.js
@@ -1,4 +1,5 @@
-(function () {
+const cellular = (function() {
+
     // Rule 73: http://atlas.wolfram.com/01/01/73/
     const rules = [
         [[1, 1, 1], 0],
@@ -62,7 +63,7 @@
         activeCells.forEach(cell => context.fillRect(cell.x, cell.y, cellWidth, cellHeight));
     }
 
-    function leftPad(padChar, str, length) {
+    function _leftPad(padChar, str, length) {
         const charsNeeded = length - str.length;
         if (charsNeeded <= 0) return str;
         let pad = '';
@@ -74,12 +75,12 @@
 
     function convertRowToHex(row) {
         const numberOfBytes = Math.ceil(row.length/8);
-        const rowString = leftPad('0', row.join(''), 8*numberOfBytes); // pad to make up to whole byte
+        const rowString = _leftPad('0', row.join(''), 8*numberOfBytes); // pad to make up to whole byte
         const binaryStrings = [];
         for (let i=0; i<rowString.length; i+=8) {
             binaryStrings.push(rowString.substring(i, i+8));
         }
-        let hexStrings = binaryStrings.map(bin => leftPad('0', parseInt(bin, 2).toString(16), 2));
+        let hexStrings = binaryStrings.map(bin => _leftPad('0', parseInt(bin, 2).toString(16), 2));
         return hexStrings.join('');
     }
 
@@ -89,7 +90,7 @@
             const hexString = hex.substring(i,i+2);
             hexStrings.push(hexString);
         }
-        const binaryStrings = hexStrings.map(hex => leftPad('0', parseInt(hex, 16).toString(2), 8));
+        const binaryStrings = hexStrings.map(hex => _leftPad('0', parseInt(hex, 16).toString(2), 8));
         const binary = binaryStrings.join('');
         return binary.substring(binary.length-rowSize, binary.length).split('');
     }
@@ -113,90 +114,5 @@
         return generateRowFunc(numberOfColumns);
     }
 
-    function main() {
-        const canvas = document.getElementById('canvas');
-        const context = canvas.getContext('2d');
-
-        const numberOfColumns = 100; // 11
-        const cellSize = Math.floor(window.innerWidth / numberOfColumns);
-        const numberOfRows = Math.floor(window.innerHeight / cellSize);  //100; // 6
-        canvas.width = cellSize * numberOfColumns;
-        canvas.height = cellSize * numberOfRows;
-
-        const params = new URLSearchParams(location.search.slice(1));
-        const qsInitialValue = params.get('initialValue');
-        const initialRow = getRowFromQueryStringOrDefault(qsInitialValue, numberOfColumns, (n) => generateRow(n));
-        const hex = convertRowToHex(initialRow);
-        const $initialValue = $('#initialValue');
-        $initialValue.val(hex);
-        $initialValue.change(function() {
-            const value = $(this).val();
-            const isEnabled = isValidInitialValue(numberOfColumns, value);
-            //console.log(new Date(), `goButton enabled=${isEnabled} value=${value}`);
-            $('#goButton').prop('disabled', !isEnabled);
-        });
-
-        const rows = [];
-        for (let r = 0; r < numberOfRows; r++) {
-            let row;
-            if (r === 0) {
-                row = initialRow;
-            } else {
-                row = advanceRow(rows[r - 1]);
-            }
-            rows.push(row);
-        }
-
-        let isMenuActive = false;
-        let lastMoveTime = performance.now();
-
-        let timeStep = 100; // speed = 1 cell / second
-        let lastTime = null;
-
-        let counter = 0;
-        let mainLoop = (currentTime, deltaTime) => {
-            counter++;
-            if (isMenuActive && (currentTime - lastMoveTime)>2000) {
-                console.log('deactivating menu');
-                $('.menu').fadeOut();
-                isMenuActive = false;
-            }
-            if (lastTime === null) {
-                rows.push(advanceRow(rows[rows.length-1]));
-                lastTime = currentTime;
-            }
-            let timeDiff = deltaTime + (currentTime - lastTime);
-            while (timeDiff >= timeStep) {
-                timeDiff -= timeStep;
-                rows.shift();
-                rows.push(advanceRow(rows[rows.length-1]));
-            }
-            let yOffSet = Math.floor(timeDiff*(cellSize/timeStep));
-            draw(context, rows, cellSize, cellSize, yOffSet);
-
-            lastTime = currentTime;
-            if (counter < 1000) {
-                window.requestAnimationFrame( (t) => mainLoop(t, timeDiff));
-            } else {
-                console.log('finished');
-            }
-        };
-        mainLoop(null, 0);
-
-        $(document).mousemove(function() {
-            //console.log('moved', isMenuActive);
-            const currentTime = performance.now();
-            const elapsedTime = currentTime - lastMoveTime;
-            if (!isMenuActive) {
-                //console.log('activating menu');
-                $('.menu').fadeIn();
-                isMenuActive = true;
-            }
-            lastMoveTime = currentTime;
-        });
-    }
-
-    $(document).ready(function() {
-        main();
-    });
+    return { generateRow, applyRules, advanceRow, draw, convertRowToHex, convertHexToRow, isValidInitialValue, getRowFromQueryStringOrDefault };
 })();
